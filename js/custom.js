cd3612104a8710c0633349f8c449d9f656cab961
MahiAkram <mahiakram2002@gmail.com>
2021-09-09 18:12:05 +0600

    3rd update

--- js/custom.js
+++ js/custom.js
@@ -15,4 +15,32 @@ $(function(){
         delay: 10,
         time: 6000
     });
+
+    $(window).scroll(function(){
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > 550){
+            $('#btp').fadeIn();
+        }
+        else{
+            $('#btp').fadeOut();
+        }
+
+        // menu fix
+        if (scrolling > 350){
+            $('.navbar').addClass('menu-fix');
+        }
+        else{
+            $('.navbar').removeClass('menu-fix');
+        }
+
+    });
+
+    // back to top
+
+    $('#btp').click(function(){
+        $('html,body').animate({
+            scrollTop : 0,
+        },1000);
+    });
 });
